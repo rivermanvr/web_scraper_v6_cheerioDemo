ed1a194c721ef582475f457525ebe9311e8ae78c
rivermanvr <vrgithub@riversconsulting.com>
2017-10-27 15:44:21 -0400

    adding modules for front end development

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require( 'express' );
 const app = express();
-const path = require('path');
-const bodyParser = require('body-parser')
+const path = require( 'path' );
+const bodyParser = require( 'body-parser' );
 const morgan = require( 'morgan' );
 const routes = require( './routes/api' );
+
+const request = require( 'request-promise' );
+const fs = require( 'fs' );
+const cheerio = require( 'cheerio' );
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/dist', express.static(path.join(__dirname, 'dist')));
@@ -15,6 +19,51 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use('/api', routes);
+
+let url = 'http://www.google.com';
+const file = fs.createWriteStream('result.html')
+
+// test #1   ...request site and log the result (non-promise call)
+
+// request(url, function (error, response, body) {
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+
+// test #2   ...request site and log the result (promise call)
+
+// request(url)
+//   .then(result => console.log(result))
+//   .catch(err => console.log('vin error: ', err));
+
+// test #3   ...request site and save the result (promise call)
+
+// request(url)
+// .then(result => file.write(result))
+// .catch(err => console.log('vin error: ', err));
+
+// test #3   ...request site and save the result (promise call)
+
+// request(url)
+// .then(result => file.write(result))
+// .catch(err => console.log('vin error: ', err));
+
+// test #4   ...request site and use cheerio (promise call)
+
+url = 'https://www.indeed.com/viewjob?jk=abe3124266f27566&q=redux+react&l=New+York%2C+NY&tk=1btfije3b1bt42e0&from=web';
+
+request(url)
+.then(result => cheerio.load(result))
+.then($ => {
+  const jobTitle = $('.jobtitle font').text();
+  console.log('>>>>>>>>>Our Results: ')
+  console.log(jobTitle)
+})
+.catch(err => console.log('vin error: ', err));
+
+
+/*******************************/
 
 app.get('/', (req, res, next)=> res.sendFile(path.join(__dirname, 'public/index.html')));
 
