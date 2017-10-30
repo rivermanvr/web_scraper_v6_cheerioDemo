1a0ec5ae79105f974d47c4c4256bbd03177355eb
rivermanvr <vrgithub@riversconsulting.com>
2017-10-30 14:20:00 -0400

    presentation app is finished

--- src/components/future.js
+++ src/components/future.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 export default function MVP() {
   return (
-    <div>Hello Future</div>
+    <div className="marginT container-fluid">
+      <div className="row">
+        <div className="col-sm-8">
+        <h3>Where do we go from here?:</h3>
+          <ul className="tabRight">
+            <li className="fontS23H20 backWhite padTBSMM well">Incorporate scripting ( phantomjs &/or possibly selenium ) </li>
+            <li className="fontS23H20 backWhite padTBSMM well">Add database functionalty ( current app is a demo )</li>
+            <li className="fontS23H20 backWhite padTBSMM well">Add sign-in & navigate functionality</li>
+            <li className="fontS23H20 backWhite padTBSMM well">generalize the interface</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/use_case.js
+++ src/components/use_case.js
@@ -4,11 +4,11 @@ export default function Step1() {
   return (
     <div className="marginT container-fluid">
       <div className="row">
-        <div className="col-sm-10">
+        <div className="col-sm-11">
         <h3>What is it?:</h3>
           <ul className="tabRight">
-            <li className="fontS23H20 marginB">Web scraping (web harvesting, web data extraction &/or data scraping) is used for extracting data from websites. It is a form of copying, in which specific data is gathered and copied from the web, typically into a central local database or spreadsheet, for later retrieval or analysis. [1]</li>
-            <li className="fontS23H20 marginB">Generally, this is done with software that simulates human Web surfing to collect specified bits of information from different websites. [2]</li>
+            <li className="fontS23H20 backWhite padTBSMM well">Web scraping (web harvesting, web data extraction &/or data scraping) is used for extracting data from websites. It is a form of copying, in which specific data is gathered and copied from the web, typically into a central local database or spreadsheet, for later retrieval or analysis. [1]</li>
+            <li className="fontS23H20 backWhite padTBSMM well">Generally, this is done with software that simulates human Web surfing to collect specified bits of information from different websites. [2]</li>
           </ul>
         </div>
       </div>
@@ -24,9 +24,9 @@ export default function Step1() {
         <h3>Example Use Case:</h3>
         <h5>(These examples all require authentication)</h5>
         <ul className="tabRight">
-          <li className="fontS23H20 marginB">Automatically retrieve client data for input into our CRM when no API is available. (ex: Supplier Portal)</li>
-          <li className="fontS23H20 marginB">Pull contact data for a client into our CRM (ex: LinkedIn)</li>
-          <li className="fontS23H20 marginB">Pull job data earmarked for me down automatically. (ex: StackOverflow Jobs)</li>
+          <li className="fontS23H20 backWhite padTBSMM well">Automatically retrieve client data for input into our CRM when no API is available. (ex: Supplier Portal)</li>
+          <li className="fontS23H20 backWhite padTBSMM well">Pull contact data for a client into our CRM (ex: LinkedIn)</li>
+          <li className="fontS23H20 backWhite padTBSMM well">Pull job data earmarked for me down automatically. (ex: StackOverflow Jobs)</li>
         </ul>
       </div>
     </div>
