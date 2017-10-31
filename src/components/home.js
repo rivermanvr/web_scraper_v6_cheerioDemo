da97bff4ee7ddcb7edcfa23a36afe65267482bc0
rivermanvr <vrgithub@riversconsulting.com>
2017-10-31 16:02:55 -0400

    reordered bullets

--- src/components/home.js
+++ src/components/home.js
@@ -5,6 +5,20 @@ export default function Main() {
   return (
     <div className="marginT container-fluid">
       <div className="row">
+        <div className="col-sm-11">
+          <h3>What is it?:</h3>
+          <ul className="tabRight">
+            <li className="fontS23H20 backWhite padTBSMM well">Web scraping (web harvesting, web data extraction &/or data scraping) is used for extracting data from websites. It is a form of copying, in which specific data is gathered and copied from the web, typically into a central local database or spreadsheet, for later retrieval or analysis. [1]</li>
+            <li className="fontS23H20 backWhite padTBSMM well">Generally, this is done with software that simulates human Web surfing to collect specified bits of information from different websites. [2]</li>
+          </ul>
+        </div>
+        <div className="col-sm-12">
+          Footnotes:
+          <ul className="tabRight">
+            <li>[1] Wikipedia - https://en.wikipedia.org/wiki/Web_scraping</li>
+            <li>[2] Techopedia - https://www.techopedia.com/definition/5212/web-scraping</li>
+          </ul>
+        </div>
         <div className="col-sm-6">
         <h3>Premise:</h3>
           <ul className="tabRight">
@@ -12,15 +26,6 @@ export default function Main() {
             <li className="fontS23H20">determine a use case for it.</li>
             <li className="fontS23H20">package it - minimum viable application</li>
             <li className="fontS23H20">visualize the product evolution</li>
-          </ul>
-        </div>
-        <div className="col-sm-5">
-          <h3>Modules evaluated:</h3>
-          <ul className="tabRight">
-            <li className="fontS23H20">phantomjs & casper (automated testing)</li>
-            <li className="fontS23H20">selenium (automated testing)</li>
-            <li className="fontS23H20">jsdom</li>
-            <li className="fontS23H20">request & cheerio</li>
           </ul>
         </div>
       </div>
--- src/components/use_case.js
+++ src/components/use_case.js
@@ -4,32 +4,27 @@ export default function Step1() {
   return (
     <div className="marginT container-fluid">
       <div className="row">
-        <div className="col-sm-11">
-        <h3>What is it?:</h3>
+        <div className="col-sm-5">
+          <h3>Modules evaluated:</h3>
           <ul className="tabRight">
-            <li className="fontS23H20 backWhite padTBSMM well">Web scraping (web harvesting, web data extraction &/or data scraping) is used for extracting data from websites. It is a form of copying, in which specific data is gathered and copied from the web, typically into a central local database or spreadsheet, for later retrieval or analysis. [1]</li>
-            <li className="fontS23H20 backWhite padTBSMM well">Generally, this is done with software that simulates human Web surfing to collect specified bits of information from different websites. [2]</li>
+            <li className="fontS23H20">phantomjs & casper (automated testing)</li>
+            <li className="fontS23H20">selenium (automated testing)</li>
+            <li className="fontS23H20">jsdom</li>
+            <li className="fontS23H20">request & cheerio</li>
           </ul>
         </div>
       </div>
       <div className="row">
-      <div className="col-sm-12">
-        Footnotes:
-        <ul className="tabRight">
-          <li>[1] Wikipedia - https://en.wikipedia.org/wiki/Web_scraping</li>
-          <li>[2] Techopedia - https://www.techopedia.com/definition/5212/web-scraping</li>
-        </ul>
+        <div className="col-sm-10">
+          <h3>Example Use Case:</h3>
+          <h5>(These examples all require authentication)</h5>
+          <ul className="tabRight">
+            <li className="fontS23H20 backWhite padTBSMM well">Automatically retrieve client data for input into our CRM when no API is available. (ex: Supplier Portal)</li>
+            <li className="fontS23H20 backWhite padTBSMM well">Pull contact data for a client into our CRM (ex: LinkedIn)</li>
+            <li className="fontS23H20 backWhite padTBSMM well">Pull job data earmarked for me down automatically. (ex: StackOverflow Jobs)</li>
+          </ul>
+        </div>
       </div>
-      <div className="col-sm-10">
-        <h3>Example Use Case:</h3>
-        <h5>(These examples all require authentication)</h5>
-        <ul className="tabRight">
-          <li className="fontS23H20 backWhite padTBSMM well">Automatically retrieve client data for input into our CRM when no API is available. (ex: Supplier Portal)</li>
-          <li className="fontS23H20 backWhite padTBSMM well">Pull contact data for a client into our CRM (ex: LinkedIn)</li>
-          <li className="fontS23H20 backWhite padTBSMM well">Pull job data earmarked for me down automatically. (ex: StackOverflow Jobs)</li>
-        </ul>
-      </div>
-    </div>
     </div>
   )
 }
